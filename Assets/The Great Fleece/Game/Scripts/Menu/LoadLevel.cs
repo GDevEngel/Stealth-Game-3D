84d6ed5676168d07c00aa931c664d6c82d1ddcdd
GDevEngel <tengel403@gmail.com>
2021-08-13 17:58:05 +0200

    main menu and loading screen

---
+++ Assets/The Great Fleece/Game/Scripts/Menu/LoadLevel.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LoadLevel : MonoBehaviour
+{
+    public Image progressBar;
+
+	void Start ()
+    {
+        StartCoroutine(LoadLevelAsync());
+	}
+	
+	IEnumerator LoadLevelAsync()
+    {
+        AsyncOperation asyncOperation = SceneManager.LoadSceneAsync("Main");
+
+        while (asyncOperation.isDone != true)
+        {
+            progressBar.fillAmount = asyncOperation.progress;
+            yield return new WaitForEndOfFrame();
+        }
+    }
+}
---
+++ Assets/The Great Fleece/Game/Scripts/Menu/MainMenu.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadScene("LoadingScreen");
+        Debug.Log("start game");
+    }
+
+    public void Quit()
+    {
+        Debug.Log("quit game");
+        Application.Quit();
+    }
+}
